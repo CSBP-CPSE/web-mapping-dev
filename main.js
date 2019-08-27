a8210bed531dd756e9102ef05d571621965b2d69
staubibr <staubin.bruno@gmail.com>
2019-08-27 16:04:18 -0400

    modified a bunch of things :
    added splash screen,
    no longer uses weird plugin style workaround, just bundle the whole app
    fixed some french english stuff

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
-// import MBT from '../mapbox-tools/mbtools.js';
-MBT.Token = 'pk.eyJ1Ijoic3RhdWJiciIsImEiOiJjanpoaHAxOGcwdnYyM2JsZWxhZDVxbTdsIn0.6N2EV6uBr6fLpdJvCy4_Sw';
-MBT.Locale = document.documentElement.lang || "en";
-MBT.Nls = nls;
+import Nls from './nls.js';
+import Splash from './splash.js';
+import Map from '../mapbox-tools/tools/map.js';
+import Utils from '../mapbox-tools/tools/utils.js';
+import Factory from '../mapbox-tools/tools/factory.js';
+import Core from '../basic-tools/tools/core.js';
+import Dom from '../basic-tools/tools/dom.js';
 
-var map = MBT.Factory.Map('map', 'mapbox://styles/staubbr/cjztbdnpi0qgp1dpruzzu6ec4', [-75.6972, 45.4215], 11);
+Core.locale = document.documentElement.lang || "en";
+Core.nls = Nls;
 
-map.addControl(MBT.Factory.NavigationControl(), 'top-left');
-map.addControl(MBT.Factory.ScaleControl('metric'));
-map.addControl(MBT.Factory.LegendControl());
+Map.Token = 'pk.eyJ1Ijoic3RhdWJiciIsImEiOiJjanpoaHAxOGcwdnYyM2JsZWxhZDVxbTdsIn0.6N2EV6uBr6fLpdJvCy4_Sw';
 
-map.on('load', function(ev) {
-	// Maybe make a classification function in MBT.Map
-	var classes = [
-		'case',
-		['==', ['get', 'Data_prov'], "Microsoft"], 'rgba(0, 140, 220, 0.8)',
-		'rgba(200, 0, 40, 0.8)'
-	];
+var popup = new Splash();
 
-	var layers = ["ab", "bc", "mb", "nb", "nl", "ns", "nt", "nu", "on", "pe", "qc", "sk", "yt"];
+// TODO : Needs to be converted to promise but stupid ie11
+popup.Show(Initialize);
 
-	MBT.Map.ReorderLayers(map, layers);
-	MBT.Map.Choropleth(map, layers, classes);
+function Initialize() {	
+	var map = Factory.Map('map', 'mapbox://styles/staubbr/cjztbdnpi0qgp1dpruzzu6ec4', [-75.6972, 45.4215], 11);
 
-	layers.forEach(function(l) {
-		map.on('click', l, function(ev) {
-			if (ev.features.length == 0) return;
-		
-			var html = MBT.Utils.HTMLize(ev.features[0].properties);
+	map.addControl(Factory.NavigationControl(), 'top-left');
+	map.addControl(Factory.ScaleControl('metric'));
+	map.addControl(Factory.LegendControl());
+
+	map.on('load', function(ev) {
+		// Maybe make a classification function in MBT.Map
+		var classes = [
+			'case',
+			['==', ['get', 'Data_prov'], "Microsoft"], 'rgba(0, 140, 220, 0.8)',
+			'rgba(200, 0, 40, 0.8)'
+		];
+
+		var layers = ["ab", "bc", "mb", "nb", "nl", "ns", "nt", "nu", "on", "pe", "qc", "sk", "yt"];
+
+		Map.ReorderLayers(map, layers);
+		Map.Choropleth(map, layers, classes);
+
+		layers.forEach(function(l) {
+			map.on('click', l, function(ev) {
+				if (ev.features.length == 0) return;
 			
-			MBT.Map.InfoPopup(map, ev.lngLat, html);
+				var html = Utils.HTMLize(ev.features[0].properties);
+				
+				Map.InfoPopup(map, ev.lngLat, html);
+			});
 		});
 	});
-});
+}
--- nls.js
+++ nls.js
@@ -6,8 +6,8 @@ var nls = {
 		"fr" : "Bâtiments de Microsoft"
 	},
 	"Legend_Item_2" : {
-		"en" : "Other Buildings",
-		"fr" : "Autres bâtiments"
+		"en" : "<abbr title='Open Database of Buildings'>ODB</abbr> Buildings (Stat Can)",
+		"fr" : "Bâtiments de la <abbr title='Base de données ouverte des immeubles'>BDOI</abbr> (Stat Can)"
 	},
 	"Toc_Instruction" : {
 		"en" : "Turn layers on or off",
@@ -16,5 +16,51 @@ var nls = {
 	"Toc_Legend" : {
 		"en" : "Legend",
 		"fr" : "Légende"
+	},
+	"Label_CD" : {
+		"en" : "Census Division (CD)",
+		"fr" : "Division de recensement (DR)"
+	},
+	"Label_CSD" : {
+		"en" : "Census Subdivision (CSD)",
+		"fr" : "Sous-division de recensement (SDR)"
+	},
+	"Label_CCS" : {
+		"en" : "Census Consolidated Subdivision (CCS)",
+		"fr" : "Sous-division de recensement unifiée (SRU)"
+	},
+	"Label_DA" : {
+		"en" : "Dissemination Area (DA)",
+		"fr" : "Aire de diffusion (AD)"
+	},
+	"Splash_LODE_Title" : {
+		"en" : "LODE Viewer",
+		"fr" : "Visionneuse de l'ECDO"
+	},
+	"Splash_LODE_1" : {
+		"en" : "The Linkable Open Data Environment (LODE) is an exploratory initiative that aims at enhancing the use and harmonization of open micro data primarily from municipal, provincial and federal sources.",
+		"fr" : "L'Environnement de couplage de données ouvertes (ECDO) est une initiative exploratoire qui vise à accroître l'utilisation et l'harmonisation de microdonnées ouvertes provenant principalement de sources municipales, provinciales et fédérales."
+	},
+	"Splash_LODE_2" : {
+		"en" : "The results are a collection of datasets released under a single open data license (Open Government Licence - Canada), as well as open source tools used to process the data, and collaborations in an open space.",
+		"fr" : "Il en résulte une collection d'ensembles de données diffusés en vertu d'une même licence de données ouvertes (Licence du gouvernement ouvert – Canada), ainsi que des outils libres utilisés pour traiter les données et des collaborations dans un espace ouvert."
+	},
+	"Splash_LODE_3" : {
+		"en" : "The LODE is implemented by the Data Exploration and Integration Lab (DEIL). Thus, it is an ongoing and exploratory initiative, developed in an open space, as an open project.",
+		"fr" : "L'ECDO est mis en œuvre par le Laboratoire d'exploration et d'intégration des données. Il s'agit par conséquent d'une initiative continue et exploratoire mise sur pied dans un espace ouvert à titre de projet ouvert."
+	},
+	"Splash_WIP" : {
+		"en" : "The LODE Viewer is a work in progress. We are aware of certain issues. Notably, certain census boundaries are incorrect.",
+		"fr" : "La visionneuse de l'ECDO est un travail en cours. Nous sommes conscient de certains problèmes. Notamment, certaines limites de recensement sont incorrectes."
+	},
+	"Splash_Warning" : {
+		"en" : "Warning: This web application consumes a large amount of data. It is advised not to use it on a mobile device with a limited data plan.",
+		"fr" : "Attention : Cette application consomme un grand volume de données. Il est conseillé de ne pas l'utiliser sur une plateforme mobile avec un plan de données limité."
+	},
+	"Splash_Continue" : {
+		"en" : "CONTINUE",
+		"fr" : "CONTINUER"
 	}
 }
+
+export default nls;
