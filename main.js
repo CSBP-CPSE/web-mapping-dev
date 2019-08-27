d7a2e4034bf73c3ba830d72ea7178ca41b4a3510
staubibr <staubin.bruno@gmail.com>
2019-08-27 10:45:33 -0400

    added toc and legend, fr and en

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
+// import MBT from '../mapbox-tools/mbtools.js';
 MBT.Token = 'pk.eyJ1Ijoic3RhdWJiciIsImEiOiJjanpoaHAxOGcwdnYyM2JsZWxhZDVxbTdsIn0.6N2EV6uBr6fLpdJvCy4_Sw';
+MBT.Locale = document.documentElement.lang || "en";
+MBT.Nls = nls;
 
 var map = MBT.Factory.Map('map', 'mapbox://styles/staubbr/cjztbdnpi0qgp1dpruzzu6ec4', [-75.6972, 45.4215], 11);
 
 map.addControl(MBT.Factory.NavigationControl(), 'top-left');
 map.addControl(MBT.Factory.ScaleControl('metric'));
+map.addControl(MBT.Factory.LegendControl());
 
 map.on('load', function(ev) {
 	// Maybe make a classification function in MBT.Map
 	var classes = [
-	  'case',
-	  ['<', ['get', 'area'], 1000], 'rgba(0,194,214,0.5)',
-	  ['<', ['get', 'area'], 3000], 'rgba(0,155,229,0.5)',
-	  ['<', ['get', 'area'], 5000], 'rgba(0,115,225,0.5)',
-	  ['<', ['get', 'area'], 10000], 'rgba(0,71,196,0.5)',
-	  ['<', ['get', 'area'], 20000], 'rgba(194,6,82,0.5)',
-	  '#AD001B'
+		'case',
+		['==', ['get', 'Data_prov'], "Microsoft"], 'rgba(0, 140, 220, 0.8)',
+		'rgba(200, 0, 40, 0.8)'
 	];
 
 	var layers = ["ab", "bc", "mb", "nb", "nl", "ns", "nt", "nu", "on", "pe", "qc", "sk", "yt"];
-	
-	// MBT.Map.ReorderLayers(map, ["buildings"]);
+
+	MBT.Map.ReorderLayers(map, layers);
 	MBT.Map.Choropleth(map, layers, classes);
 
 	layers.forEach(function(l) {
---
+++ nls.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var nls = {
+	"Legend_Item_1" : {
+		"en" : "Microsoft buildings",
+		"fr" : "Bâtiments de Microsoft"
+	},
+	"Legend_Item_2" : {
+		"en" : "Other Buildings",
+		"fr" : "Autres bâtiments"
+	},
+	"Toc_Instruction" : {
+		"en" : "Turn layers on or off",
+		"fr" : "Afficher ou cachers les couches"
+	},
+	"Toc_Legend" : {
+		"en" : "Legend",
+		"fr" : "Légende"
+	}
+}
