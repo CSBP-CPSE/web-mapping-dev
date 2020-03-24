0a4851ccb78c2909539e37645b3eb974b5b574ac
aalhabashna <aalhabashna@gmail.com>
2020-03-24 15:30:44 -0400

    Updated map style to frank and addressed some comments

--- odhf-viewer/application.js
+++ odhf-viewer/application.js
@@ -59,7 +59,8 @@ export default class ProxApp {
 	AddGroup() {
 		// Top-right group for legend, etc.		
 		this.group = {
-			legend : Factory.LegendControl(this.current.Legend, this.current.Title, this.current.Subtitle)
+			legend : Factory.LegendControl(this.current.Legend, this.current.Title, this.current.Subtitle),
+			download : Factory.DownloadControl(null)
 		}
 		
 		this.map.AddControl(Factory.Group(this.group));
