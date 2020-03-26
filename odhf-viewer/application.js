314080d72468d22c229cbe13cfa3a93283066877
staubibr <bruno.st-aubin2@canada.ca>
2020-03-26 16:03:01 -0400

    version

--- odhf-viewer/application.js
+++ odhf-viewer/application.js
@@ -59,7 +59,7 @@ export default class ProxApp {
 	AddGroup() {
 		// Top-right group for legend, etc.		
 		this.group = {
-			legend : Factory.LegendControl(this.current.Legend, this.current.Title, this.current.Subtitle),
+			legend : Factory.LegendControl(this.current.Legend, null, this.current.Title, this.current.Subtitle),
 			download : Factory.DownloadControl(null)
 		}
 		
